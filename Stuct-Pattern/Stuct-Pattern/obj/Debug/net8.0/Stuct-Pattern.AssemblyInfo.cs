93f3b74bb9d0467b2792fc042b3e0750511cd8b7
Niewrsis <154369161+Niewrsis@users.noreply.github.com>
2024-12-07 22:42:10 +0300

    feat: idk what is this but let it be

--- Stuct-Pattern/Stuct-Pattern/obj/Debug/net8.0/Stuct-Pattern.AssemblyInfo.cs
+++ Stuct-Pattern/Stuct-Pattern/obj/Debug/net8.0/Stuct-Pattern.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stuct-Pattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a16dc8f5a831eccf6eee14ee6f61f96ed8a5d7dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stuct-Pattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stuct-Pattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
